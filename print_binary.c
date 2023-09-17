704181a2155f9dca31a98b701169da017cd7bc8e
ACHUX21 <1337.achraf@gmail.com>
2023-09-17 04:53:39 +0100

    Resolved issue with %c format specifier

--- print_binary.c
+++ print_binary.c
@@ -53,3 +53,29 @@ int print_binary(va_list arg)
 	}
 
 	for (; n > 0; n = n / 2)
+#include "main.h"
+/**
+ * print_binary - print binary information
+ * @arg: a pointer to a binary
+ * Return: a pointer to
+*/
+int print_binary(va_list arg)
+{
+	unsigned int n;
+	char arr[100];
+	int num = 0;
+
+	n = va_arg(arg, unsigned int);
+	if (n == 0)
+	{
+		_putchar(48);
+		num++;
+		return (num);
+	}
+
+	for (; n > 0; n = n / 2)
+	{
+		arr[num] = (n % 2) + 48;
+		num++;
+	}
+	arr[num] = '\0';
