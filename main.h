b41c7991bbbaaef4f8cd5bd92e8d0551d0df6dee
ACHUX21 <1337.achraf@gmail.com>
2023-09-17 17:11:21 +0100

    Handling %o

--- main.h
+++ main.h
@@ -28,6 +28,7 @@ int print_string(va_list arg);
 int print_integer(va_list arg);
 int print_rev(va_list arg);
 int print_binary(va_list arg);
+int print_octal(va_list arg);
 int fun_helper(const char *format, va_list arg, struct formats *format_redear);
 int reverse(char *arr);
 int print_rot(va_list arg);
--- print_binary.c
+++ print_binary.c
@@ -28,3 +28,29 @@ int print_binary(va_list arg)
 
 	return (num);
 }
+
+int print_octal(va_list arg)
+{
+	unsigned int n;
+    char arr[100];
+    int num = 0;
+
+    n = va_arg(arg, unsigned int);
+
+    if (n == 0)
+    {
+        _putchar(48);
+        num++;
+        return (num);
+    }
+
+    for (; n > 0; n = n / 8)
+    {
+        arr[num] = (n % 8) + 48;
+        num++;
+    }
+    arr[num] = '\0';
+    reverse(arr);
+
+    return (num);
+}
