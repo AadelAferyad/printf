cb18f41fd06192f944e95363fae346f3795388e0
ACHUX21 <1337.achraf@gmail.com>
2023-09-17 04:23:37 +0100

    Fixed bug causing incorrect output in certain cases

--- print_binary.c
+++ print_binary.c
@@ -16,3 +16,19 @@ int print_binary(va_list arg)
 	unsigned int n;
 	char arr[100];
 	int num = 0;
+#include "main.h"
+/**
+ * print_binary - print binary information
+ * @arg: a pointer to a binary
+ * Return: a pointer to
+*/
+int print_binary(va_list arg)
+{
+	unsigned int n;
+	char arr[100];
+	int num = 0;
+
+	n = va_arg(arg, unsigned int);
+	if (n == 0)
+	{
+		_putchar(48);
