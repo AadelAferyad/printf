2d6855c0fc396795e5a23bddf14930aa2a86b30f
ACHUX21 <1337.achraf@gmail.com>
2023-09-17 03:08:32 +0100

    Implemented support for floating-point numbers

--- main.h
+++ main.h
@@ -54,3 +54,29 @@ struct formats
 
 typedef struct formats fm;
 
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * struct formats - Struct formats
+ *
+ * @f: The specifier.
+ * @print: The function associated.
+ */
+
+struct formats
+{
+	char *f;
+	int (*print)(va_list);
+};
+
+typedef struct formats fm;
+
+int h_Integer(int n);
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _puts(char *s);
+int print_char(va_list arg);
