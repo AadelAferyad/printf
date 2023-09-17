3982ce7b110b83bc1f88b99159a4946f6a240f8c
ACHUX21 <1337.achraf@gmail.com>
2023-09-17 02:53:31 +0100

    Implemented dynamic width and precision specifiers

--- main.h
+++ main.h
@@ -33,3 +33,24 @@
 struct formats
 {
 	char *f;
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * struct formats - Struct formats
+ *
+ * @f: The specifier.
+ * @print: The function associated.
+ */
+
+struct formats
+{
+	char *f;
+	int (*print)(va_list);
+};
+
+typedef struct formats fm;
+
