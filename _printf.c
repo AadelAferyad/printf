903693eeed858855cb1f801334fe7a3ae70545c4
ACHUX21 <1337.achraf@gmail.com>
2023-09-17 06:23:45 +0100

    Resolved issue with %c format specifier

--- _printf.c
+++ _printf.c
@@ -5,3 +5,14 @@
  * @format: printf format
  * Return: int length of the inpue.
 */
+#include "main.h"
+/**
+ * _printf - printf function for printing
+ * @format: printf format
+ * Return: int length of the inpue.
+*/
+
+int _printf(const char *format, ...)
+{
+	fm format_redear[] = {{"%c", print_char}, {"%s", print_string},
+							{"%d", print_integer}, {"%i", print_integer},
