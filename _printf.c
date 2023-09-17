409345da6301c38abdf65bdba1a8f670bf65893b
ACHUX21 <1337.achraf@gmail.com>
2023-09-17 07:08:48 +0100

    Fixed edge case with string formatting

--- _printf.c
+++ _printf.c
@@ -53,3 +53,29 @@ int _printf(const char *format, ...)
 
 	return (len); /*removed (len - 1)*/
 }
+#include "main.h"
+/**
+ * _printf - printf function for printing
+ * @format: printf format
+ * Return: int length of the inpue.
+*/
+
+int _printf(const char *format, ...)
+{
+	fm format_redear[] = {{"%c", print_char}, {"%s", print_string},
+							{"%d", print_integer}, {"%i", print_integer},
+							{"%b", print_binary}, {"%r", print_rev},
+							{"%R", print_rot}, {NULL, NULL}};
+	va_list arg;
+	int len = 0;
+
+	va_start(arg, format);
+	len = fun_helper(format, arg, format_redear);
+
+	return (len); /*removed (len - 1)*/
+}
+/**
+ * fun_helper - helper function for printingd
+ * @format: string
+ * @arg: string
+ * @format_redear: string
