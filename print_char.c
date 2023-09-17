ec6da9adf3661ecd9498126db1dda399238617ad
ACHUX21 <1337.achraf@gmail.com>
2023-09-17 05:38:42 +0100

    Added error handling for invalid format specifiers

--- print_char.c
+++ print_char.c
@@ -5,3 +5,14 @@
  * @arg: char to print
  * Return: 1.
 */
+#include "main.h"
+/**
+ * print_char - print a character
+ * @arg: char to print
+ * Return: 1.
+*/
+int print_char(va_list arg)
+{
+	char str;
+
+	str = va_arg(arg, int);
