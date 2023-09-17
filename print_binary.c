9920ac157a26068b85a8e8a281b09c1ad32f602b
ACHUX21 <1337.achraf@gmail.com>
2023-09-17 04:38:38 +0100

    Implemented dynamic width and precision specifiers

--- print_binary.c
+++ print_binary.c
@@ -32,3 +32,24 @@ int print_binary(va_list arg)
 	if (n == 0)
 	{
 		_putchar(48);
+#include "main.h"
+/**
+ * print_binary - print binary information
+ * @arg: a pointer to a binary
+ * Return: a pointer to
+*/
+int print_binary(va_list arg)
+{
+	unsigned int n;
+	char arr[100];
+	int num = 0;
+
+	n = va_arg(arg, unsigned int);
+	if (n == 0)
+	{
+		_putchar(48);
+		num++;
+		return (num);
+	}
+
+	for (; n > 0; n = n / 2)
