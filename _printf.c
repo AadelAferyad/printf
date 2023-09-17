4718f8da06749370b4698883d03c5a58e0355028
ACHUX21 <1337.achraf@gmail.com>
2023-09-17 06:38:46 +0100

    Optimized memory usage in printf function

--- _printf.c
+++ _printf.c
@@ -16,3 +16,19 @@ int _printf(const char *format, ...)
 {
 	fm format_redear[] = {{"%c", print_char}, {"%s", print_string},
 							{"%d", print_integer}, {"%i", print_integer},
+#include "main.h"
+/**
+ * _printf - printf function for printing
+ * @format: printf format
+ * Return: int length of the inpue.
+*/
+
+int _printf(const char *format, ...)
+{
+	fm format_redear[] = {{"%c", print_char}, {"%s", print_string},
+							{"%d", print_integer}, {"%i", print_integer},
+							{"%b", print_binary}, {"%r", print_rev},
+							{"%R", print_rot}, {NULL, NULL}};
+	va_list arg;
+	int len = 0;
+
