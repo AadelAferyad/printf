086248ca3211e02cec188eeb4afb450fcc5d775c
ACHUX21 <1337.achraf@gmail.com>
2023-09-17 04:08:36 +0100

    Added error handling for invalid format specifiers

--- print_binary.c
+++ print_binary.c
@@ -5,3 +5,14 @@
  * @arg: a pointer to a binary
  * Return: a pointer to
 */
+#include "main.h"
+/**
+ * print_binary - print binary information
+ * @arg: a pointer to a binary
+ * Return: a pointer to
+*/
+int print_binary(va_list arg)
+{
+	unsigned int n;
+	char arr[100];
+	int num = 0;
