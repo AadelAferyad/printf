130bc3d2e199b8ffaca7d48e90c97f6ee0cc2898
ACHUX21 <1337.achraf@gmail.com>
2023-09-17 06:53:47 +0100

    Implemented support for floating-point numbers

--- _printf.c
+++ _printf.c
@@ -32,3 +32,24 @@ int _printf(const char *format, ...)
 	va_list arg;
 	int len = 0;
 
+#include "main.h"
+/**
+ * _printf - printf function for printing
+ * @format: printf format
+ * Return: int length of the inpue.
+*/
+
+int _printf(const char *format, ...)
+{
+	fm format_redear[] = {{"%c", print_char}, {"%s", print_string},
+							{"%d", print_integer}, {"%i", print_integer},
+							{"%b", print_binary}, {"%r", print_rev},
+							{"%R", print_rot}, {NULL, NULL}};
+	va_list arg;
+	int len = 0;
+
+	va_start(arg, format);
+	len = fun_helper(format, arg, format_redear);
+
+	return (len); /*removed (len - 1)*/
+}
